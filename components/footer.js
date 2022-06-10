f117cdeeb968b2683e234a1a0d4d588805cce43f
Dallin Moak <dallinmoak@gmail.com>
2022-06-10 15:53:10 -0600

    add navlinks, modular css

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,9 @@
+import styles from '../styles/footer.module.scss'
+
 export default function footer(){
   return(
-    <footer>
-      <div className="footer-content">
+    <footer className={styles['main-footer']}>
+      <div className={styles['footer-content']}>
         This site is an unofficial ward resource and is not endorsed by or a product of <a href="https://www.churchofjesuschrist.org" target="_blank" rel="noreferrer">The Church of Jesus Christ of Latter Day Saints</a>
       </div>
     </footer>
