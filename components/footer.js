5f17cc43db4ada8be6aae8cbdf2e728ba10682bc
Dallin Moak <dallinmoak@gmail.com>
2022-06-10 10:20:35 -0600

    style the footer, also make it mobile responsive

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,9 @@
 export default function footer(){
   return(
     <footer>
-      footer
+      <div className="footer-content">
+        This site is an unofficial ward resource and is not endorsed by or a product of <a href="https://www.churchofjesuschrist.org" target="_blank" rel="noreferrer">The Church of Jesus Christ of Latter Day Saints</a>
+      </div>
     </footer>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <div>
-        <h1>Hello world</h1>
+       <h1>Hello world</h1>
       </div>
     </div>
   )
