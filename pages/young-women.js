0eb28d5bed4079d8c88c2f7473c125990cd96282
Dallin Moak <dallinmoak@gmail.com>
2022-06-22 23:23:18 -0600

    fix image styling
    updated the image styling to work better on mobile and not make everything all wanky

--- pages/young-women.js
+++ pages/young-women.js
@@ -17,7 +17,9 @@ export default function YoungWomen() {
       {messages.map((message, index) => {
         return (
           <div className={styles['message-item']}key={index}>
-            {message.image? <Image src={message.image} layout="raw"/>: null}
+            <div className={styles['img-container']}>
+              {message.image? <Image src={message.image}/>: null}
+            </div>
             <div className={styles['message-text']}>
               {message.header?<h3>{message.header}</h3>: null}
               {message.content?<p>{message.content}</p>: null}
