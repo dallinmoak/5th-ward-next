081200ca2dd95b916415d6faa2b87909f6de135b
Dallin Moak <dallinmoak@gmail.com>
2022-06-24 16:22:30 -0600

    experimental configuration

--- common/nav-list.js
+++ common/nav-list.js
@@ -1,9 +1,9 @@
 const navList = [
-  {name:'Home', route:'/'},
+  {name:'Home', route:'/', navbar: true},
+  {name:'Calendar', route: '/calendar', navbar: true},
   {name:'Elder\'s Quorum', route:'/elders-quorum', navbar: true},
   {name:'Relief Society', route: '/relief-society', navbar: true},
   {name:'Bishopric', route: '/bishopric', navbar: true},
-  {name:'Calendar', route: '/calendar', navbar: true},
   {name:'Young Men', route: '/young-men', navbar: true},
   {name:'Young Women', route: '/young-women', navbar: true},
   {name:'Sunday School', route: '/sunday-school', navbar: true},
