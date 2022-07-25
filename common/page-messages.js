e0e2d7db226e0a5dffc12004e109f4cc45a10038
dallinmoak <dallinmoak@gmail.com>
2022-07-24 19:43:54 -0600

    update page messages

--- common/page-messages.js
+++ common/page-messages.js
@@ -9,10 +9,7 @@ const messages = [
   },
   {
     content: [
-      'July 4th- Combined Ward Activity',
-      'July 7th- Ward Temple Day',
-      'Ciudad Juarez Temple is closed: May 31-June 20',
-      'Deacon\'s Camp is July 14 -16',
+      'On Sunday July 31st there will be a Seminary Fireside at the Stake Center at 7pm.  This is for all seminary students and their parents.',
     ],
     pages: ['Bishopric']
   },
@@ -39,10 +36,18 @@ const messages = [
     pages: ["Elder's Quorum"]
   },
   {
+    content: [
+      'Summer is almost over and the new school year is right around the corner.  With the start of the new school year comes a new year of Seminary.  The Stake needs our assistance with security to keep the students and instructors safe.  If you have the ability to help with Seminary security please fill out the form at the following link.',
+      'https://forms.gle/t6PWccLLABEWtEuu5',
+    ],
+    pages: ['Elder\'s Quorum']
+  },
+  {
     header: "Upcoming Activities:",
     content: [
-      "Family History Workshop - 1st & 3rd Wednesdays at the church. Come learn about family history and get help from our ward family history consultants.",
-      "Family Game Night - The last Friday each month is family game night at the church at 7:00 p.m.  Bring a favorite game and a favorite dessert to share and enjoy playing games with your fellow ward members.  Invite your friends.  Our next game night is June 24th.",
+      "Family History Workshop - 1st & 3rd Wednesday's at the church.  Come learn about family history and get help from our ward family history consultants.",
+      "Family Game Night - The last Friday each month is family game night at the church at 7:00 p.m.  Bring a favorite game and a favorite dessert to share and enjoy playing games with your fellow ward members.  Invite your friends.  Our next game night is Friday, July 29th.",
+      'Ward Temple Trip - August 20th we will be taking a trip to the Gila Valley Temple.  We have booked two baptimal sessions for the youth and there is a 12:00 endowment session for those that would like to reserve a spot visit the church website.'
     ],
     image: null,
     pages: ['Elder\'s Quorum'],
