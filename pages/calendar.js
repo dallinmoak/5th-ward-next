7100fb43f7aa30cd80a3b8e54a29f78e0d55e1af
Dallin Moak <dallinmoak@gmail.com>
2022-08-13 15:03:45 -0600

    remove calNames prop

--- pages/calendar.js
+++ pages/calendar.js
@@ -6,7 +6,6 @@ import GetCalendars from '../components/cal/get-calendars';
 
 export default function Calendar(props) {
   const nav = navItem("Calendar")
-  const calNames = ['Youth','Deacons','Teachers','Preists','YW 12-13','YW 14-18','Young Men','Young Women', 'Relief Society', "Elder's Quorum", "Primary" ];
 
   return (
     <div className={styles['container']}>
@@ -14,7 +13,7 @@ export default function Calendar(props) {
       <p className={[styles['bottom-margin'],styles['message-link-container']].join(' ')}>
         The official ward calendar is available at <a href="https://www.churchofjesuschrist.org/calendar" target="_blank" rel="noreferrer">churchofjesuschrist.org</a>.
       </p>
-      <GetCalendars calNames={calNames} includeList={props.includeList} setModal={props.setModal}/>
+      <GetCalendars includeList={props.includeList} setModal={props.setModal}/>
     </div>
   );
 }
