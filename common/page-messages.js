02ac2eac88524d568613c39cecc5149669c9578b
Dallin Moak <dallinmoak@gmail.com>
2022-11-27 10:30:49 -0700

    Update page-messages.js

--- common/page-messages.js
+++ common/page-messages.js
@@ -8,24 +8,6 @@ const messages = [
     pages: ["Young Women", "Young Men"]
   },
   {
-    content: [
-      'On Sunday July 31st there will be a Seminary Fireside at the Stake Center at 7pm.  This is for all seminary students and their parents.',
-    ],
-    pages: ['Bishopric']
-  },
-  {
-    header: 'Our Sunday School room assignments are as follows:',
-    content: [
-      'Gospel Doctrine- RS Room',
-      'Course 16/17- Room 101',
-      'Course 14/15- Room 102A',
-      'Course 12/13- Stage',
-      'Course 11- Room 102'
-    ],
-    styles: ['left','bullet'],
-    pages: ['Bishopric']
-  },
-  {
     header: 'Quorum Presidency',
     content: [
       'Jim Huff - 915-471-4928',
@@ -34,24 +16,6 @@ const messages = [
       'Greg McDonald - 915-539-4215'
     ],
     pages: ["Elder's Quorum"]
-  },
-  {
-    content: [
-      'Summer is almost over and the new school year is right around the corner.  With the start of the new school year comes a new year of Seminary.  The Stake needs our assistance with security to keep the students and instructors safe.  If you have the ability to help with Seminary security please fill out the form at the following link.',
-      'https://forms.gle/t6PWccLLABEWtEuu5',
-    ],
-    pages: ['Elder\'s Quorum']
-  },
-  {
-    header: "Upcoming Activities:",
-    content: [
-      "Family History Workshop - 1st & 3rd Wednesday's at the church.  Come learn about family history and get help from our ward family history consultants.",
-      "Family Game Night - The last Friday each month is family game night at the church at 7:00 p.m.  Bring a favorite game and a favorite dessert to share and enjoy playing games with your fellow ward members.  Invite your friends.  Our next game night is Friday, July 29th.",
-      'Ward Temple Trip - August 20th we will be taking a trip to the Gila Valley Temple.  We have booked two baptimal sessions for the youth and there is a 12:00 endowment session for those that would like to reserve a spot visit the church website.'
-    ],
-    image: null,
-    pages: ['Elder\'s Quorum'],
-    styles: ['left', 'bullet']
   },
   {
     content: [
