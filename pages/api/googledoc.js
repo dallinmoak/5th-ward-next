903e4ba8edad68e165ec1ca917e9c8e60520d614
Dallin Moak <dallinmoak@gmail.com>
2023-03-12 03:42:41 +0000

    mvp

--- pages/api/googledoc.js
+++ pages/api/googledoc.js
@@ -8,3 +8,37 @@
 //abstract the stuff in the token to an env variable somehow
 
 //then auth on the server with that.
+
+const util = require('util');
+const {google} = require('googleapis');
+const {authenticate} = require('@google-cloud/local-auth');
+
+const handler = async (req, res) =>{
+  let creds = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
+  //need to make the newlines newlinier
+  Object.keys(creds).forEach(key=>{
+    creds[key] = creds[key].split(String.raw`\n`).join('\n')
+  })
+
+  // console.log("Client email:",creds.client_email);
+  const auth = new google.auth.GoogleAuth({
+    credentials: creds,
+  scopes: ['https://www.googleapis.com/auth/documents']
+});
+
+  google.options({auth});
+
+  const docs = google.docs('v1');
+
+  const docRes = await docs.documents.get({
+    documentId: '1SsoOb5Tv2NymjUnTcTQ2u5YVJoF-MmlUOr9dvsoT6Oc',
+  });
+
+  console.log(util.inspect(docRes.data, false, 17));
+  // return res.data;
+  
+  res.status(200).json("yee-haw");
+}
+
+export default handler;
