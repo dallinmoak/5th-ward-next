6af42647cf1c82888201fed78c46d675cec80214
Dallin Moak <dallinmoak@gmail.com>
2023-03-20 21:15:37 -0600

    add survey link

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,8 @@ export default function Home() {
         The El Paso 5th Ward of the El Paso Mount Franklin Stake of the Church
         of Jesus Christ of Latter Day Saints
       </em>
+      <h2>!NEW!</h2>
+      <p>please click <a href="https://docs.google.com/forms/d/e/1FAIpQLSfcF3SklQhhkJoQJABv3H-3oSokWWakZk9XOFCKKDavp4p6ig/viewform?usp=sf_link" target="_blank" rel="noreferrer">here</a> to take the ward technology survey. Thanks!</p>
       <h3 className={styles['home-text']}>Quick Links</h3>
       <div className={pageStyles['quick-links']}>
         {quickLinks.map((link, index) => {
